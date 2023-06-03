8f84b23b7ef0e89e09e40e1fbea9b8b7e154a000
Lisa Mbuwa <mbuwalisa@gmail.com>
2023-06-03 14:56:38 +0200

    edited

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-/*let array1 = [];
 
-function add () {
-    let value = document.getElementById('input').value;
-    array1.push (value);
-    document.getElementById('output').innerHTML= array1;
-
-}*/
-document.querySelector("#wrapper").onclick = function () {
-    if (document.querySelector("#input").value.length == 0) {
-      alert("Please Enter a Errand");
-    } else {
-      document.querySelector("#errands").innerHTML += `
-                <div class="task">
-                    <button class="done">
-                    </button>
-                    <span id="name">
-                        ${document.querySelector("#input").value}
-                    </span>
-                    <button class="remove">
-                        <img class="redx" src="https://i.postimg.cc/K860PG8c/gie5-B478-T-removebg-preview.png"/>
-                    </button>
-                </div>
-            `;
-      let putted = document.querySelectorAll(".remove");
-      for (let i = 0; i < putted.length; i++) {
-        putted[i].onclick = function () {
-          this.parentNode.remove();
-        };
-      }
+document.querySelector("#addbtn").onclick = function () {
+  let line = document.querySelector("#errands");
+  if (document.querySelector("#input").value.length == 0) {
+    alert("Please Enter a Task");
+  } else {
+    document.querySelector("#errands").innerHTML += `
+              <div class="task">
+                  <button class="done" onclick='strike()'>
+                  </button>
+                  <span id="name">
+                      ${document.querySelector("#input").value}
+                  </span>
+                  <button class="remove">
+                      <img class="redx" src="https://i.postimg.cc/K860PG8c/gie5-B478-T-removebg-preview.png"/>
+                  </button>
+              </div>
+          `;
+    let putted = document.querySelectorAll(".remove");
+    for (let i = 0; i < putted.length; i++) {
+      putted[i].onclick = function () {
+        this.parentNode.remove();
+      };
     }
+    line.addEventListener(("click"), function (){
+      line.style.textDecoration = "line-through";
+    })
+  }
 };
+// function strike() {
+//  let ele = document.getElementsByClassName("name");
+//  ele.style.textDecoration = "line-through";
+// }
+// let list = document.getElementsByClassName("task");
+// list.addEventListener(
+//   "click",
+//   function (ev) {
+//     if (ev.target.tagName === "span") {
+//       ev.target.classList.toggle("checked");
+//     }
+//   },
+//   false
+// );
+
+
 //Load task from local storage when the page loads
 //document.addEventListener(" DOMContentLoaded",enter errands);
 
